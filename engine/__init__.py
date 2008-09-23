c522f09998437e88305ba906d45a7a88d1752040
brosner <brosner@gmail.com>
2008-09-23 14:53:25 -0600

    Added a default queue and allow an explicit queue to be passed in to defer.

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,10 +1,12 @@
 
-from engine.queues import QueueItem
+from engine.queues import DummyQueue, QueueItem
 
-queue = None
+default_queue = DummyQueue()
 
-def defer(func, *args):
+def defer(func, *args, queue=None):
     """
     Queues the callable with the passed in arguments.
     """
+    if queue is None:
+        queue = default_queue
     queue.put(QueueItem(func, args))
