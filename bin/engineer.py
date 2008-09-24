ffd128441960bd3ba34e601909a05655b41d59ce
brosner <brosner@gmail.com>
2008-09-24 12:30:08 -0600

    Beginning forms of a script to process the queue called engineer.py.

---
+++ bin/engineer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import time
+import stomp
+import logging
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+class QueueListener(object):
+    def on_error(self, headers, message):
+        logging.info(message)
+    
+    def on_message(self, headers, message):
+        queue_item = pickle.loads(message)
+        queue_item.execute()
+
+def main():
+    logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+    conn = stomp.Connection()
+    conn.add_listener(QueueListener())
+    conn.start()
+    conn.connect()
+    conn.subscribe(destination="/queue/hello_world", ack="auto")
+    while True:
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
