674e1b2ae0c0ea21476f110b5e08aa988b160721
Brian Rosner <brosner@gmail.com>
2009-01-04 02:09:11 -0700

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name = "queue_engine",
+    version = "0.1.0pre",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    url = "http://github.com/brosner/engine/tree/master",
+    packages = [
+        "engine",
+    ],
+)
